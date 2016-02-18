da24bb327748d9dc8d05744a87a669a95caed11e
substanceof <substance-of@ya.ru>
2016-02-19 00:58:06 +0300

    exceptions

--- source/vkapi.d
+++ source/vkapi.d
@@ -2,13 +2,20 @@ module vkapi;
 
 import std.stdio, std.net.curl, std.conv, std.string, std.json;
 
+
+
 class VKapi{
+
+// ===== API & networking =====
 
     const string vkurl = "https://api.vk.com/method/";
     const string vkver = "5.45";
     string vktoken = "";
 
     this(string token){
+        if(token.length != 85){
+            throw BackendException("Invalid token length");
+        }
         vktoken = token;
     }
 
@@ -19,7 +26,7 @@ class VKapi{
 
     JSONValue vkget(string meth, string[string] params, bool dontRemoveResponse = false){
         bool rmresp = dontRemoveResponse;
-        auto url = vkurl ~ meth ~ "?";
+        auto url = vkurl ~ meth ~ "?"; //so blue
         foreach(key; params.keys) {
             auto val = params[key];
             url ~= key ~ "=" ~ val ~ "&"; //todo fix val for url
@@ -29,8 +36,12 @@ class VKapi{
 
         if(resp.type == JSON_TYPE.OBJECT) {
             if("error" in resp){
-                writeln("vk api error:"); //todo exceptions
-                writeln(resp.toPrettyString);
+
+                auto eobj = resp["error"];
+                auto emsg = ("error_text" in eobj) ? eobj["error_text"].str : eobj["error_msg"].str;
+                auto ecode = eobj["error_code"].uinteger;
+                throw ApiErrorException(emsg, ecode);
+
             } else {
                 rmresp = false;
             }
@@ -39,4 +50,29 @@ class VKapi{
         return rmresp ? resp : resp["response"];
     }
 
+}
+
+// ===== Exceptions =====
+
+class BackendException : Exception {
+    public {
+        @safe pure nothrow this(string message,
+                                string file =__FILE__,
+                                size_t line = __LINE__,
+                                Throwable next = null) {
+            super(message, file, line, next);
+        }
+    }
+}
+
+class ApiErrorException : Exception {
+    public {
+        @safe pure nothrow this(string message,
+                                int error_code,
+                                string file =__FILE__,
+                                size_t line = __LINE__,
+                                Throwable next = null) {
+            super(message, file, line, next);
+        }
+    }
 }
