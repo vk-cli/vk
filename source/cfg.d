cfce19ee64b9863f1f0dc1063c4a05ba7ca7767d
HaCk3D <vladstudio@rambler.ru>
2016-02-19 14:18:15 +0300

    config file storage

---
+++ source/cfg.d
@@ -0,0 +1,27 @@
+module cfg;
+
+import std.path, std.stdio, std.file, std.string;
+
+string[string] storage;
+
+string[string] load() {
+  auto config = expandTilde("~/.vkrc");
+  if (config.exists) {
+    auto f = File(config, "r");
+    while (!f.eof) {
+      auto line = f.readln.strip;
+      if (line.length != 0) storage[line[0..line.indexOf("=")-1]] = line[line.indexOf("=")+2..$];
+    }
+    f.close;
+  }
+  return storage;
+}
+
+void save(string[string] stor) {
+  auto config = expandTilde("~/.vkrc");
+  auto f = File(config, "w");
+  foreach(key, value; stor) {
+    f.write(key ~ " = " ~ value ~ "\n");
+  }
+  f.close;
+}
