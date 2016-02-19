db863a51b445d21c1aae404b2f025137a467ea06
substanceof <substance-of@ya.ru>
2016-02-19 22:39:03 +0300

    token check + users.get wrapper

--- source/vkapi.d
+++ source/vkapi.d
@@ -6,19 +6,41 @@ import std.net.curl, std.uri, std.json;
 import utils;
 
 
+// ===== API objects =====
+
+struct vkUser{
+    string first_name;
+    string last_name;
+    int id;
+}
+
 class VKapi{
 
 // ===== API & networking =====
 
-    const string vkurl = "https://api.vk.com/method/";
+    private const string vkurl = "https://api.vk.com/method/";
     const string vkver = "5.45";
-    string vktoken = "";
+    private string vktoken = "";
+    bool isTokenValid;
+    vkUser me;
 
     this(string token){
-        if(token.length != 85){
-            throw new BackendException("Invalid token length");
-        }
         vktoken = token;
+        isTokenValid = checkToken(token);
+    }
+
+    private bool checkToken(string token){
+        if(token.length != 85) return false;
+        try{
+            me = usersGet();
+        } catch (ApiErrorException e) {
+            dbm("ApiErrorException: " ~ e.msg);
+            return false;
+        } catch (BackendException e){
+            dbm("BackendException: " ~ e.msg);
+            return false;
+        }
+        return true;
     }
 
     string httpget(string addr) {
@@ -45,6 +67,7 @@ class VKapi{
             url ~= key ~ "=" ~ val.encode ~ "&";
         }
         url ~= "v=" ~ vkver ~ "&access_token=" ~ vktoken;
+        dbm("request: " ~ url);
         JSONValue resp = httpget(url).parseJSON;
 
         if(resp.type == JSON_TYPE.OBJECT) {
@@ -61,6 +84,26 @@ class VKapi{
         }
 
         return rmresp ? resp : resp["response"];
+    }
+
+    // ===== API method wrappers =====
+
+    vkUser usersGet(int userId = 0, string fields = "", string nameCase = "nom"){
+        string[string] params;
+        if(userId != 0) params["user_ids"] = userId.to!string;
+        if(fields != "") params["fields"] = fields;
+        if(nameCase != "nom") params["name_case"] = nameCase;
+        auto resp = vkget("users.get", params);
+
+        if(resp.array.length != 1) throw new BackendException("users.get (one user) fail: response array length != 1");
+        resp = resp[0];
+
+        vkUser rt = {
+            id:resp["id"].integer.to!int,
+            first_name:resp["first_name"].str,
+            last_name:resp["last_name"].str
+        };
+        return rt;
     }
 
 }
