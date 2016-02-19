ab4241365ec3aacc9bf0fcab7bf2ccd1536d15f9
HaCk3D <vladstudio@rambler.ru>
2016-02-19 14:18:50 +0300

    save/load token

--- source/app.d
+++ source/app.d
@@ -3,36 +3,42 @@
 import deimos.ncurses.ncurses;
 import std.string, std.stdio, std.process, std.conv;
 import core.stdc.locale;
-import vkapi;
+import vkapi, cfg;
 
 void init() {
   setlocale(LC_CTYPE,"");
   initscr();
 }
 
+void print(string s) {
+  s.toStringz.printw;
+}
+
+VKapi get_token() {
+  char token;
+  "Insert your access token here: ".print;
+  spawnShell(`xdg-open "http://oauth.vk.com/authorize?client_id=5110243&scope=friends,wall,messages,audio,offline&redirect_uri=blank.html&display=popup&response_type=token" >> /dev/null`);
+  getstr(&token);
+  auto strtoken = (cast(char*)&token).to!string;
+  storage["token"] = strtoken;
+  return new VKapi(strtoken);
+}
+
+VKapi set_token(string token) {
+  "set".print;
+  return new VKapi(token);
+}
+
 void main(string[] args) {
   init;
-  scope(exit) endwin;
+  scope(exit)    endwin;
+  scope(failure) endwin();
 
-  printw("Insert your access token here: ");
-
-  spawnShell(`xdg-open "http://oauth.vk.com/authorize?client_id=5110243&scope=friends,wall,messages,audio,offline&redirect_uri=blank.html&display=popup&response_type=token" >> /dev/null`);
-
-  char token;
-
-  getstr(&token);
+  auto storage = load;
+  auto api = "token" in storage ? set_token(storage["token"]) : get_token;
+  api.vkget("messages.getDialogs", ["count": "1", "offset": "0"]).toPrettyString.print;
 
   refresh;
-
-  token.writeln;
-
-  //auto api = new VKapi(token.to!string);
-  //api.vkget("messages.getDialogs", ["count": "1", "offset": "0"]).toPrettyString.writeln();
-
-  auto stoken = (cast(char*)&token).to!string;
-  auto api = new VKapi(stoken);
-  api.vkget("messages.getDialogs", ["count": "1", "offset": "0"]).toPrettyString.writeln();
-
-
   getch;
+  storage.save;
 }
