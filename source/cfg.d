5673dc081c561cac8e32f7a1be6a5df7b9536945
HaCk3D <vladstudio@rambler.ru>
2016-02-19 22:02:24 +0300

    newstorage -> storage

--- source/cfg.d
+++ source/cfg.d
@@ -3,17 +3,17 @@ module cfg;
 import std.path, std.stdio, std.file, std.string;
 
 string[string] load() {
-  string[string] newstorage;
+  string[string] storage;
   auto config = expandTilde("~/.vkrc");
   if (config.exists) {
     auto f = File(config, "r");
     while (!f.eof) {
       auto line = f.readln.strip;
-      if (line.length != 0) newstorage[line[0..line.indexOf("=")-1]] = line[line.indexOf("=")+2..$];
+      if (line.length != 0) storage[line[0..line.indexOf("=")-1]] = line[line.indexOf("=")+2..$];
     }
     f.close;
   }
-  return newstorage;
+  return storage;
 }
 
 void save(string[string] stor) {
