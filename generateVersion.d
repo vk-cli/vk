a31ecccce567ea96c298cd3db87c25fb82b350f4
substanceof <vk-cli.dev@ya.ru>
2016-05-17 20:16:20 +0300

    vkversion template

--- generateVersion.d
+++ generateVersion.d
@@ -8,8 +8,20 @@ void main() {
     lastCommitHash = matchFirst(executeShell("git log -1").output, regex(r"(?:^commit\s+)([0-9a-f]{40})"))[1][0..7],
     currentBranch = matchFirst(executeShell("git status").output, regex(r"(?:^On branch\s+)(.+)"))[1];
 
+  auto verTemplate = "
+module vkversion;
+
+const string
+  currentVersion = \"master\";
+
+";
+
   auto fileName = "source/vkversion.d";
-  auto text = readText(fileName).split("\n");
+  string[] text;
+
+  if(!exists(fileName)) text = verTemplate.split("\n");
+  else text = readText(fileName).split("\n");
+
   auto reg = regex("(^\\s*currentVersion\\s*=\\s*\")(.+)(\"\\s*;)");
   foreach (ref line; text) {
     auto match = matchFirst(line, reg);
