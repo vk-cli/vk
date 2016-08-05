d03075fc966393a76345c32493ed9177740edf2f
HaCk3D <vladstudio@rambler.ru>
2016-08-05 22:02:18 +0300

    init, exit, ui

--- source/app.d
+++ source/app.d
@@ -1,32 +1,56 @@
-import std.stdio, std.conv, std.string, std.regex, std.array, std.datetime,
-    std.random, core.time;
-import std.exception, core.exception, std.process;
-import std.net.curl, std.uri, std.json;
-import std.range, std.algorithm;
-import std.parallelism, std.concurrency, core.thread, core.sync.mutex;
-import vkapi, logic, cfg, utils, localization;
+import  std.stdio, std.conv, std.string, std.regex, std.array, std.random,
+        std.datetime, core.time,
+        std.exception, core.exception, std.process,
+        std.net.curl, std.uri, std.json,
+        std.range, std.algorithm,
+        std.parallelism, std.concurrency, core.thread, core.sync.mutex;
+
+import deimos.ncurses.ncurses;
+import ui, controller, vkapi, logic, cfg, utils, localization, vkversion;
 
 string[string] config;
 
+void init() {
+  updateGcSignals;
+  initdbm;
+  localize;
+  initscr;
+  curs_set(0);
+  noecho;
+  config = load;
+}
+
+void Exit(string msg = "") {
+  endwin;
+  if (msg != "") {
+    string
+      boldRed   = "\033[31m\x1b[1m",
+      boldWhite = "\033[39m\x1b[1m",
+      resetAttr = "\x1b[0m";
+    (boldRed ~ "Error: " ~ boldWhite ~ msg ~ resetAttr).writeln;
+  }
+  exit(0);
+}
+
 void main(string[] args) {
-    updateGcSignals;
-    initdbm;
-    localize;
-    config = load;
-
-    auto token = config["token"];
-    auto api = new MainProvider(token);
-
-    auto usersView = api.friendsList.getView(30, 80);
-
-    while (usersView.empty) Thread.sleep(dur!"msecs"(500));
-
-    foreach (e; usersView) {
-        e.fullName.writeln;
+  foreach(e; args) {
+    if (e == "-v" || e == "-version") {
+      writefln("vk-cli %s", currentVersion);
+      exit(0);
     }
+  }
+  init;
 
-    auto usersInfo = api.getInfo(list.friends);
-    writeln("[ users are updated: " ~ usersInfo.isUpdated.to!string ~ " ]");
+  auto api = new MainProvider(config["token"]);
 
-    exit(0);
+  auto usersView = api.friendsList.getView(30, 80);
+  while (usersView.empty) Thread.sleep(dur!"msecs"(500));
+  foreach (e; usersView) (e.fullName ~ "\n").print;
+
+  auto usersInfo = api.getInfo(list.friends);
+  print("[ users are updated: " ~ usersInfo.isUpdated.to!string ~ " ]");
+
+  getch;
+
+  Exit("kakaya-to ebola slomalas'");
 }
