d9f7bfad40cf1459f6ef72023d062d2f78a563c6
substanceof <substance-of@ya.ru>
2016-12-20 14:13:31 +0300

    logs

--- source/musicplayer.d
+++ source/musicplayer.d
@@ -253,6 +253,7 @@ class mpv: Thread {
 
 
   private void req(string cmd) {
+    dbm("mpv <- " ~ cmd);
     if(!isInit) {
       dbm("mpv - req: noinit");
       return;
@@ -305,6 +306,7 @@ class mpv: Thread {
 
   private void mpvhandle(string rc) {
     try {
+      dbm("mpv: " ~ rc);
       auto m = parseJSON(rc);
       if(m.type != JSON_TYPE.OBJECT) return;
       if(
