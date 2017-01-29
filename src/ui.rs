b4e4efed367318c2de563e8db57934f71e43c6de
HaCk3D <vladstudio@rambler.ru>
2017-01-29 12:21:12 +0300

    loop for key testing

--- src/ui.rs
+++ src/ui.rs
@@ -4,42 +4,42 @@ use std::char;
 use self::ncurses::*;
 
 pub fn render() {
-  let locale_conf = LcCategory::all;
-  setlocale(locale_conf, "en_US.UTF-8");
+  setlocale(LcCategory::all, "en_US.UTF-8");
   initscr();
   raw();
 
-  /* Allow for extended keyboard (like F1). */
+  // Allow for extended keyboard (like F1).
   keypad(stdscr(), true);
   noecho();
 
-  printw("Enter a character: ");
+  printw("Enter any character (q to exit):\n");
 
-  let ch = get_wch();
-  match ch {
-    Some(WchResult::KeyCode(KEY_MOUSE)) => {}
+  'main: loop {
+    let ch = get_wch();
+    match ch {
+      Some(WchResult::KeyCode(KEY_MOUSE)) => {}
 
-    Some(WchResult::KeyCode(c)) => {
-      attron(A_BOLD() | A_BLINK());
-      printw(&format!("\n{}", c));
-      attroff(A_BOLD() | A_BLINK());
-      printw(" pressed");
+      Some(WchResult::KeyCode(c)) => {
+        attron(A_BOLD() | A_BLINK());
+        printw(&format!("\n{}", c));
+        attroff(A_BOLD() | A_BLINK());
+        printw(" pressed");
+      }
+
+      Some(WchResult::Char(c)) => {
+        printw("Key pressed: ");
+        attron(A_BOLD() | A_BLINK());
+        printw(&format!("{} - ", c));
+        if c == 113 { break 'main; }
+        printw(format!("{}\n", char::from_u32(c as u32).expect("Invalid char")).as_ref());
+        attroff(A_BOLD() | A_BLINK());
+      }
+
+      None => {}
     }
 
-    Some(WchResult::Char(c)) => {
-      printw("\nKey pressed: ");
-      attron(A_BOLD() | A_BLINK());
-      printw(format!("{}\n", char::from_u32(c as u32).expect("Invalid char")).as_ref());
-      attroff(A_BOLD() | A_BLINK());
-    }
-
-    None => {}
+    refresh(); 
   }
-
-  /* Refresh, showing the previous message. */
-  refresh();
-
-  nocbreak();
-  getch();
+  // getch();
   endwin();
 }
