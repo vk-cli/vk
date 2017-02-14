230f8c1dbebf034de97b3b793dbeadd7b69c859c
substanceof <substance-of@ya.ru>
2017-02-14 03:01:54 +0300

    move common functionality to linear-cache struct

--- src/worker_utils.rs
+++ src/worker_utils.rs
@@ -1,4 +1,3 @@
-use workers::*;
 use errors::*;
 use utils::DummyResult;
 use std::time::Duration;
@@ -7,6 +6,9 @@ use futures::Future;
 use futures::future::BoxFuture;
 use tokio_timer::Timer;
 use robots::actors::*;
+
+use workers::*;
+use workers::GetChunkAnswer::*;
 
 pub fn fork<T, E>(f: BoxFuture<Vec<T>, E>, timeout: Duration, context: &ActorCell, pos: Pos, error_prefix: &'static str)
   where T: Clone + Send + Sync + 'static,
@@ -36,4 +38,67 @@ pub fn fork<T, E>(f: BoxFuture<Vec<T>, E>, timeout: Duration, context: &ActorCel
       );
       Ok(())
     });
+}
+
+pub enum Work {
+  DownloadingAtPos(Pos),
+  Idle
+}
+
+pub struct Linear<T> {
+  pub work: Work,
+  pub cache: Vec<T>,
+  pub cache_full: bool,
+  pub logpref: &'static str
+}
+
+impl<T: Clone> Linear<T> {
+  pub fn get_chunk<F>(&mut self, pos: Pos, size: Pos, f: F) -> GetChunkAnswer<Vec<T>>
+    where F: Fn() -> () {
+
+    let reqrange = (pos + size) as usize;
+    let upos = pos as usize;
+
+    if self.cache_full {
+      CacheFull
+    } else if reqrange < self.cache.len() {
+      HasChunk(self.cache[upos..reqrange].to_vec())
+    } else {
+      match self.work {
+        Work::Idle if upos <= self.cache.len() => {
+          f();
+          self.work = Work::DownloadingAtPos(pos);
+          debug!("{} task started at pos {}", self.logpref, pos);
+          NoChunk
+        },
+        _ => NoChunk
+      }
+    }
+  }
+
+  pub fn recv_chunk(&mut self, pos: Pos, chunk: Vec<T>) {
+    let upos = pos as usize;
+    let pref = self.logpref;
+    match self.work {
+      Work::DownloadingAtPos(p)
+      if pos == p && upos <= self.cache.len() => {
+        if chunk.len() == 0 {
+          info!("{} cache full (received empty chunk)", pref);
+          self.cache_full = true;
+        } else {
+          let mut ln = self.cache.len();
+          while upos < ln {
+            self.cache.remove(ln - 1);
+            ln = self.cache.len();
+          }
+          self.cache.extend_from_slice(&chunk[..]);
+        }
+        debug!("{} chunk received to pos {}({})", pref, pos, chunk.len());
+        self.work = Work::Idle;
+      },
+      _ =>
+        warn!("{} incorrect chunk \
+              \npos: {}, lcache: {}", pref, pos, self.cache.len())
+    }
+  }
 }
--- src/workers.rs
+++ src/workers.rs
@@ -31,17 +31,6 @@ pub enum GetChunkAnswer<T> {
   CacheFull
 }
 
-enum Work {
-  DownloadingAtPos(Pos),
-  Idle
-}
-
-struct Linear<T> {
-  work: Work,
-  cache: Vec<T>,
-  cache_full: bool
-}
-
 struct FriendsWorker {
   chunk_size: i32,
   api: Arc<Api>,
@@ -57,7 +46,8 @@ impl FriendsWorker {
         Linear {
           work: Work::Idle,
           cache: Vec::new(),
-          cache_full: false
+          cache_full: false,
+          logpref: "friends:"
         }
       )
     }
@@ -66,79 +56,41 @@ impl FriendsWorker {
 
 impl Actor for FriendsWorker {
   fn receive(&self, msg: Box<Any>, context: ActorCell) {
-    const PREF: Rstr = "friends:";
     type T = User;
-    if let (Ok(msg), Ok(mut c))
-    = (Box::<Any>::downcast::<M<T>>(msg), self.main.lock()) {
+    if let (Ok(msg), Ok(mut c)) = (Box::<Any>::downcast::<M<T>>(msg), self.main.lock()) {
+      let pref = c.logpref;
       match msg {
         box M::GetChunkAtPos(pos) => {
-          let reqrange = (pos + self.chunk_size) as usize;
-          let upos = pos as usize;
-
-          let answ = if c.cache_full {
-            CacheFull
-          } else if reqrange < c.cache.len() {
-            HasChunk(c.cache[upos..reqrange].to_vec())
-          } else {
-            match c.work {
-              Work::Idle if upos <= c.cache.len() => {
-                let apiwork = self.api
-                  .as_ref()
-                  .friends_get(self.chunk_size, pos)
-                  .boxed();
-
-                fork(apiwork, Duration::from_secs(4000), &context, pos, &PREF);
-                c.work = Work::DownloadingAtPos(pos);
-                debug!("{} task started at pos {}", PREF, pos);
-                NoChunk
-              },
-              _ => NoChunk
-            }
+          let cs = self.chunk_size;
+          let get = || {
+            let apiwork = self.api
+              .as_ref()
+              .friends_get(cs, pos)
+              .boxed();
+            fork(apiwork, Duration::from_secs(4000), &context, pos, &pref);
           };
+          let answ = c.get_chunk(pos, cs, get);
           context.complete(context.sender(), answ)
         },
 
         box M::PWorkTimedOut(wp) => {
           match c.work {
             Work::DownloadingAtPos(p) if wp == p => {
-              warn!("{} task timed out at pos {}", PREF, p);
+              warn!("{} task timed out at pos {}", pref, p);
               c.work = Work::Idle
             },
             _ =>
               warn!("{} late timeout \
-              with pos: {}", PREF, wp)
+              with pos: {}", pref, wp)
           }
         },
 
         box M::PChunkReceived(pos, chunk) => {
-          let upos = pos as usize;
-          match c.work {
-            Work::DownloadingAtPos(p)
-            if pos == p && upos <= c.cache.len() => {
-              if chunk.len() == 0 {
-                info!("{} cache full (received empty chunk)", PREF);
-                c.cache_full = true;
-              } else {
-                let mut ln = c.cache.len();
-                while upos < ln {
-                  c.cache.remove(ln - 1);
-                  ln = c.cache.len();
-                }
-                c.cache.extend_from_slice(&chunk[..]);
-              }
-              debug!("{} chunk received to pos {}({})", PREF, pos, chunk.len());
-              c.work = Work::Idle;
-            },
-            _ =>
-              warn!("{} incorrect chunk \
-              \npos: {}, lcache: {}", PREF, pos, c.cache.len())
-          }
-        },
-
-        _ => ()
+          c.recv_chunk(pos, chunk);
+        }
       }
     } else {
-      error!("{} can't acquire lck / downcast message", PREF);
+      error!("friends: can't acquire lck / downcast message");
     }
   }
 }
