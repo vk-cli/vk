a12ead45b495e1583bef33c2fa367ce31e9bfd73
substanceof <substance-of@ya.ru>
2017-05-23 16:31:58 +0300

    mark 0.7.6

--- generateVersion.d
+++ generateVersion.d
@@ -21,8 +21,8 @@ import std.process, std.stdio, std.string, std.algorithm,
        std.file, std.regex, std.conv;
 
 void main() {
-  const versionNum = "0.7.5";
-  const releaseFlag = false;
+  const versionNum = "0.7.6";
+  const releaseFlag = true;
   string
     lastCommitHash,
     currentBranch;
